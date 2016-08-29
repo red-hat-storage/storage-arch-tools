b86a13359d4950520499eecc2830c064d36b2b75
Dustin Black <dblack@redhat.com>
2016-08-29 13:46:48 -0700

    reversed 'server' and 'client' variables because it was confusing

--- iperfbenchmark.sh
+++ iperfbenchmark.sh
@@ -4,44 +4,49 @@
 
 # We automatically store our outputs in git repo branches. The repo should already exist at the below path.
 repopath=/path/to/git/repo/for/results/output
+
 # We typically keep benchmark binaries in /root/bin
 PATH=$PATH:/root/bin
+
 # How many times to run each test
 iterations=1
+
 # Whether to run the test in bidirectional mode. If your servers and clients lists overlap, then you might want to set this to false.
 bidirectional=true
 
-# Starting host numeration for clients (c) and servers (s)
-c=0
+# Note: The "servers" will run the iperf3 server daemon, and the "clients" will run the iperf3 client commands to connect to those daemons. Think of the "servers" as destination and the "clients" as source.
+
+# Starting host numeration for iperf servers (s) and clients (c)
 s=0
+c=0
 
 # Hostname prefixes
-cprefix="client"
 sprefix="server"
+cprefix="client"
 
-# Total numbers of clients and servers
-numclients=12
-numservers=6
+# Total numbers of iperf servers and clients
+numservers=12
+numclients=6
 
-# Calculation of last client and server host numbers. This assumes integer enumeration of hosts (ie client{0..11}).
-lastclient=$(echo "$c+$numclients-1" | bc)
+# Calculation of last iperf server and client host numbers.
+# This assumes integer enumeration of hosts (ie server{0..11}).
 lastserver=$(echo "$s+$numservers-1" | bc)
+lastclient=$(echo "$c+$numclients-1" | bc)
 
 
-# In this example, we have 6 server nodes named server0 through server5
-# In this example, we have 12 client nodes named client0 through client11
-for server in $(seq ${s} ${lastserver}); do
-  # Run one-to-one full mesh tests
-  for client in $(seq ${c} ${lastclient}); do
-    # We don't want to test against ourself
-    if [ "${cprefix}${client}" != "${sprefix}${server}" ]; then
-      echo "Beginning test from ${sprefix}${server} to ${cprefix}${client}..."
+# For each client system
+for client in $(seq ${c} ${lastclient}); do
+  # Run one-to-one full mesh tests with servers
+  for server in $(seq ${s} ${lastserver}); do
+    # But we don't want to test against ourself
+    if [ "${sprefix}${server}" != "${cprefix}${client}" ]; then
+      echo "Beginning test from ${cprefix}${client} to ${sprefix}${server}..."
       # We use a test naming convention with "sections" broken by -- which we parse below into our git branches
       testname=iperf--bidirectional-n${server}-c${client}--1-10gbe-10-min-10-proc
       tool=`echo ${testname} | awk -F-- '{print $1}'`
       test=`echo ${testname} | awk -F-- '{print $2}'`
       testconfig=`echo ${testname} | awk -F-- '{print $3}'`
-      workload="/usr/bin/iperf3 -c ${cprefix}${client} -i 60 -t 600 -P 10"
+      workload="/usr/bin/iperf3 -c ${sprefix}${server} -i 60 -t 600 -P 10"
 
       cd ${repopath}
       git checkout ${tool}/${test}/${testconfig} 2>/dev/null || git checkout -b ${tool}/${test}/${testconfig} master
@@ -49,12 +54,12 @@ for server in $(seq ${s} ${lastserver}); do
       i=0
       while [ $i -lt ${iterations} ]; do
         # Ensure iperf3 server process is runnning
-        ssh root@${cprefix}${client} "pkill -9 iperf; pkill -9 iperf3; /usr/bin/iperf3 -s -D"
+        ssh root@${sprefix}${server} "pkill -9 iperf; pkill -9 iperf3; /usr/bin/iperf3 -s -D"
 
         # Initiate workload
         cmd="${workload}"
         if [ "${bidirectional}" = true ]; then cmd="${cmd} && ${workload} -R"; fi
-        ssh -t root@${sprefix}${server} "$cmd" | tee -a ${testname}-$(date +%F-%H-%M-%S).results
+        ssh -t root@${cprefix}${client} "$cmd" | tee -a ${testname}-$(date +%F-%H-%M-%S).results
 
         i=$[$i+1]
       done
@@ -64,12 +69,12 @@ for server in $(seq ${s} ${lastserver}); do
     else
       echo "Skipping test against self..."
     fi
-    client=$[$client+1]
+    server=$[$server+1]
   done
-  server=$[$server+1]
+  client=$[$client+1]
 done
 
 # Stop all iperf server processes
-for client in $(seq ${c} ${lastclient}); do
-  ssh root@${cprefix}${client} "pkill -9 iperf; pkill -9 iperf3"
+for server in $(seq ${s} ${lastserver}); do
+  ssh root@${sprefix}${server} "pkill -9 iperf; pkill -9 iperf3"
 done
