95d761be999a79b30fb29dec4e0c6428a4c4c3ff
Dustin Black <dblack@redhat.com>
2016-08-29 10:26:28 -0700

    moved iperf3 server start from a loop at the beginning of the script to imbedded in the iterations to ensure that the servers are definitely running when the clients initiate the workloads

--- iperfbenchmark.sh
+++ iperfbenchmark.sh
@@ -9,7 +9,7 @@ PATH=$PATH:/root/bin
 # How many times to run each test
 iterations=1
 
-# Starting numeration for clients (c) and servers (s)
+# Starting host numeration for clients (c) and servers (s)
 c=0
 s=0
 
@@ -26,31 +26,30 @@ lastclient=$(echo "$c+$numclients-1" | bc)
 lastserver=$(echo "$s+$numservers-1" | bc)
 
 
-# In this test, we have 12 client nodes named client0 through client11
-for client in $(seq ${c} ${lastclient}); do
-  #start iperf server processes
-  ssh root@${cprefix}${client} "pkill -9 iperf; pkill -9 iperf3; /usr/bin/iperf3 -s -D"
-done
-
-# In this test, we have 6 server nodes named server0 through server5
+# In this example, we have 6 server nodes named server0 through server5
+# In this example, we have 12 client nodes named client0 through client11
 for server in $(seq ${s} ${lastserver}); do
   # Run one-to-one full mesh tests
   for client in $(seq ${c} ${lastclient}); do
-    # We use a naming convention with "sections" broken by -- which we parse below into our git branches
-    testname=iperf--bidirectional-n${server}-c${client}--1-10gbe-to-1-1gbe-10-min-10-proc
+    # We use a test naming convention with "sections" broken by -- which we parse below into our git branches
+    testname=iperf--bidirectional-n${server}-c${client}--1-10gbe-10-min-10-proc
     tool=`echo ${testname} | awk -F-- '{print $1}'`
     test=`echo ${testname} | awk -F-- '{print $2}'`
     testconfig=`echo ${testname} | awk -F-- '{print $3}'`
     workload="/usr/bin/iperf3 -c ${cprefix}${client} -i 60 -t 600 -P 10"
-    #workload="/usr/bin/iperf3 -c ${cprefix}${client} -i 60 -t 10 -P 10"
 
     cd ${repopath}
     git checkout ${tool}/${test}/${testconfig} 2>/dev/null || git checkout -b ${tool}/${test}/${testconfig} master
 
     i=0
     while [ $i -lt ${iterations} ]; do
+      # Ensure iperf3 server process is runnning
+      ssh root@${cprefix}${client} "pkill -9 iperf; pkill -9 iperf3; /usr/bin/iperf3 -s -D"
+
+      # Initiate workload
       cmd="${workload} && ${workload} -R"
       ssh -t root@rhosd${server} "$cmd" | tee -a ${testname}.results
+
       i=$[$i+1]
     done
 
@@ -64,5 +63,4 @@ done
 for client in $(seq ${c} ${lastclient}); do
   #stop iperf server processes
   ssh root@${cprefix}${client} "pkill -9 iperf; pkill -9 iperf3"
-  #ssh root@${cprefix}${client} "hostname; ps -ef | grep iperf"
 done
