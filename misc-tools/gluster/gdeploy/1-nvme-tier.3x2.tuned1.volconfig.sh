301cc147ea4afb7ec1cc71caf3c0641ac48fad9f
Dustin Black <dblack@redhat.com>
2016-10-18 11:55:20 -0700

    corrected brick path in 1-nvme-tier.3x2.tuned1.volconfig.sh

--- misc-tools/gluster/gdeploy/1-nvme-tier.3x2.tuned1.volconfig.sh
+++ misc-tools/gluster/gdeploy/1-nvme-tier.3x2.tuned1.volconfig.sh
@@ -2,6 +2,6 @@
 
 # This is intended to be run on one gluster node after the preconfig gdeploy configs are run
 
-gluster volume tier 1nvme-distrep3x2 attach replica 2 n0:/rhgs/bricks/1nvme-distrep3x2-hot n1:/rhgs/bricks/1nvme-distrep3x2-hot n2:/rhgs/bricks/1nvme-distrep3x2-hot n3:/rhgs/bricks/1nvme-distrep3x2-hot n4:/rhgs/bricks/1nvme-distrep3x2-hot n5:/rhgs/bricks/1nvme-distrep3x2-hot 
+gluster volume tier 1nvme-distrep3x2 attach replica 2 n0:/rhgs/hotbricks/1nvme-distrep3x2-hot n1:/rhgs/hotbricks/1nvme-distrep3x2-hot n2:/rhgs/hotbricks/1nvme-distrep3x2-hot n3:/rhgs/hotbricks/1nvme-distrep3x2-hot n4:/rhgs/hotbricks/1nvme-distrep3x2-hot n5:/rhgs/hotbricks/1nvme-distrep3x2-hot 
 
 gluster volume tier 1nvme-distrep3x2 start force
